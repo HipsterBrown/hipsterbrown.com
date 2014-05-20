104436b25064a7e3e9d17fd0a0b6549b4a72d0af
HipsterBrown <headhipster@hipsterbrown.com>
2014-05-19 23:18:18 -0400

    added hip-feel switcher and mail functionality using Mandrill

---
+++ js/app.js
@@ -0,0 +1,75 @@
+var hipArray = ['ironic', 'authentic', 'vintage', 'classic', 'hip', 'confident'],
+  headSpan = $('span.hip-feel');
+
+$(function() {
+  'use strict';
+
+  console.log('ready to work!');
+
+  /***
+    Switches header span on page load
+  ***/
+
+  function spanSwitch(el, arr) {
+		var span = el;
+		var newWord = arr[Math.floor(Math.random() * arr.length)];
+		span.text(newWord);
+	}
+
+  spanSwitch(headSpan, hipArray);
+
+  /***
+    Mandrill Email Contact Stuff
+  ***/
+
+  var mail = new mandrill.Mandrill('nhtBwMOLtPtaBV8fKHiuww'),
+    submit = $('#submit');
+
+  function createParams(name, email, topic, message, to) {
+    var params = {
+      'message': {
+        'from_email': email,
+        'to': [{'email': to}],
+        'subject': topic,
+        'text': name + ' has sent you a message from your site: \n' + message
+      }
+    };
+
+    return params;
+  }
+
+  function clearVals() {
+    var args = arguments;
+
+    for (var i = 0; i < args.length; i++ ) {
+      args[i].val('');
+    }
+
+  }
+
+  submit.on('click', function(e) {
+    e.preventDefault();
+
+    var name = $('#name'),
+      email = $('#email'),
+      topic = $('#topic'),
+      message = $('#message'),
+      me = 'headhipster@hipsterbrown.com';
+
+    console.log(name.val(), email.val(), topic.val(), message.val(), me);
+
+    var params = createParams(name.val(), email.val(), topic.val(), message.val(), me);
+
+    mail.messages.send(params, function(res) {
+      console.log(res);
+      clearVals(name, email, topic, message);
+      $('p.response').html('<strong>Message received, thanks for reaching out!</strong>');
+    }, function(err) {
+      console.log(err);
+      $('p.response').html('<strong>Something went wrong. I\'ll look into it. \n' + err.message + '</strong>');
+    });
+  });
+
+
+
+});
