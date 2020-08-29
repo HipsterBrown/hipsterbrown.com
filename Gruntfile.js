4e8b12ef0111b21793943b635993ae96daacfe8b
HipsterBrown <headhipster@hipsterbrown.com>
2020-08-29 16:22:38 -0400

    chore: use node-sass to build css during deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
-module.exports = function(grunt) {
+const sass = require("node-sass");
 
-  'use strict';
+module.exports = function (grunt) {
+  "use strict";
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     jshint: {
       options: {
@@ -12,77 +13,67 @@ module.exports = function(grunt) {
         eqeqeq: true,
         validthis: true
       },
-      all: [
-        'js/app.js'
-      ]
+      all: ["js/app.js"]
     },
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         mangle: false
       },
       build: {
-        src: ['js/app.js'],
-        dest: 'js/app.min.js'
+        src: ["js/app.js"],
+        dest: "js/app.min.js"
       }
     },
 
     sass: {
       dist: {
         files: {
-          'css/main.min.css': [
-            'sass/app.scss'
-          ]
+          "css/main.min.css": ["sass/app.scss"]
         },
         options: {
-          style: 'compressed',
-          sourcemap: false
+          implementation: sass,
+          outputStyle: "compressed",
+          sourceMap: false
         }
       }
     },
 
     svgstore: {
       options: {
-        prefix: 'icon-',
+        prefix: "icon-"
       },
       files: {
-        'images/social-icons.svg': ['svg/*.svg']
+        "images/social-icons.svg": ["svg/*.svg"]
       }
     },
 
     watch: {
       sass: {
-        files: [
-          'sass/*.scss',
-          'sass/**/*.scss',
-          'sass/**/**/*.scss'
-        ],
-        tasks: ['sass']
+        files: ["sass/*.scss", "sass/**/*.scss", "sass/**/**/*.scss"],
+        tasks: ["sass"]
       },
 
       js: {
-        files: [
-          'js/app.js'
-        ],
-        tasks: ['jshint', 'uglify']
+        files: ["js/app.js"],
+        tasks: ["jshint", "uglify"]
       }
     }
-
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-svgstore');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-svgstore");
+  grunt.loadNpmTasks("grunt-sass");
 
   // Default task(s).
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask("default", ["watch"]);
 
-  grunt.registerTask('svg', ['svgstore']);
+  grunt.registerTask("svg", ["svgstore"]);
 
-  grunt.registerTask('production', ['sass', 'jshint', 'uglify']);
-
+  grunt.registerTask("production", ["sass", "jshint", "uglify"]);
 };
