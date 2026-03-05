3c73045c737f9bc5e45da026edc1de80c32880ff
HipsterBrown <headhipster@hipsterbrown.com>
2026-03-05 11:37:38 -0500

    feat: remove type-filter custom element, replaced by static pages

--- js/app.ts
+++ js/app.ts
@@ -3,4 +3,3 @@ import "./_elements/random-word-element";
 import "./_elements/fit-text-element";
 import "./_elements/scroll-nav-element";
 import "./_elements/theme-toggle-element";
-import "./_elements/type-filter-element";
